515e8bb940519cc2f6ab6a265a62ad34cdd01099
stodgers <stodgersm@gmail.com>
2017-08-17 22:13:33 +0800

    111

--- learn.py
+++ learn.py
@@ -930,3 +930,21 @@ od['x'] = 3
 print(od)
 print(list(od.keys()))
 print(list(od.values()))
+
+class LastUpdatedOrderedDict(OrderedDict):
+
+    def __init__(self, capacity):
+        super(LastUpdatedOrderedDict, self).__init__()
+        self._capacity = capacity
+
+    def __setitem__(self, key, value):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
